442b26f63a189bdad0b86842bcc5e219fd71e1a4
hariharan-nt <39533087+hariharan-nt@users.noreply.github.com>
2018-06-04 19:48:41 +0530

    final commit
    update the master branch with the new updates.

--- ToDo List Project/assets/js/todos.js
+++ ToDo List Project/assets/js/todos.js
@@ -1,14 +1,33 @@
 
 //Checking off ToDo List
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //
 
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
-})
+});
+
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		if($("input").val() === ""){
+			alert("Enter a task to add");
+		}else{
+			var newTodo =  $("input").val();
+			$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "
+				+newTodo+"</li>");
+			$(this).val("");
+		}
+	}
+});
+
+$(".fa-plus").on("click",function(){
+	$("input[type='text']").fadeToggle();
+});
+
